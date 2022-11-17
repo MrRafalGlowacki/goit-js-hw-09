68d6ab72165f0bad047a5b287e31185a5810cf26
Rafał Głowacki <107753644+MrRafalGlowacki@users.noreply.github.com>
2022-11-17 18:12:10 +0100

    commit

--- src/src/js/01-color-switcher.js
+++ src/src/js/01-color-switcher.js
@@ -1,1 +1,8 @@
+const start = document.querySelector("[data-start]");
+const stop = document.querySelector("[data-stop]");
+console.log(start)
+console.log(stop)
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
