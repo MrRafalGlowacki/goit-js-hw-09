b9714937a742e77837a04ebccfe2cc7d7f4e73d3
Rafał Głowacki <107753644+MrRafalGlowacki@users.noreply.github.com>
2022-11-20 20:36:16 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,14 @@
+import Notiflix from 'notiflix';
+const Btn = document.querySelector('button');
+const inDeley = document.querySelector('[name="delay"]');
+const inStep = document.querySelector('[name="step"]');
+const inAmount = document.querySelector('[name="amount"]');
+console.log(inDeley)
+console.log(inStep)
+console.log(inAmount)
+console.log(Btn)
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
